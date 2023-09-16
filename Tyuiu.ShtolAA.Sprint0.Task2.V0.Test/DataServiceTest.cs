1b8d68acba27733d272437636aa579e75530d3ba
ShtolAleksandra <aleksandrastol8@gmail.com>
2023-09-16 16:14:16 +0500

    Task2

---
+++ Tyuiu.ShtolAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShtolAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ShtolAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+           var name = "Александра";
+           var res = DataService.GetMessage(name);
+           Assert.AreEqual("Привет, Александра", res);
+        }
+    }
+}
